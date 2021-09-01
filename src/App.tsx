fde68024ca163dad9106794348e3ee169d58a555
Wali Queiroz <wali.santos@supermercadonow.com>
2021-09-01 07:46:18 -0300

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import { AuthContextProvider } from "./contexts/AuthContext";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
+
+function App() {
+  return (
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+        </Switch>
+      </BrowserRouter>
+    </AuthContextProvider>
+  );
+}
+
+export default App;
