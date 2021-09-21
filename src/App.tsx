95046f8f67309b3fbaa097bc8b77a9b69bec6563
Wali Queiroz <wali.santos@supermercadonow.com>
2021-09-21 07:59:39 -0300

    chore: put routes in separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import { ToastContainer } from 'react-toastify';
 import { AuthContextProvider } from './contexts/AuthContext';
 
 import 'react-toastify/dist/ReactToastify.css';
 
-import { Home } from './pages/Home';
-import { NewRoom } from './pages/NewRoom';
-import { Room } from './pages/Room';
-import { AdminRoom } from './pages/AdminRoom';
-import { Login } from './pages/Login';
+import Routes from './routes';
 
 const App: React.FC = () => (
   <AuthContextProvider>
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/rooms/new" component={NewRoom} />
-        <Route path="/rooms/:id" component={Room} />
-        <Route path="/admin/rooms/:id" component={AdminRoom} />
-      </Switch>
+      <Routes />
       <ToastContainer autoClose={3000} />
     </BrowserRouter>
   </AuthContextProvider>
